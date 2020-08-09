710a684dbe383932df151e0d2fa831b4fddd4966
Maciev <apcpc951995@gmail.com>
2020-08-09 14:31:39 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am the chat page</h1>
+          </Route>
+          <Route path="/">
+            <TinderCards />
+          </Route>
+        </Switch>
+      </Router>
+
+      {/*Tinder Cards*/}
+
+      {/*Buttons below tinder cards */}
+
+      {/*Chats screen*/}
+
+      {/*Individual chat screen*/}
     </div>
   );
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+
+function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: "steve jobs",
+      url:
+        "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg",
+    },
+    {
+      name: "mark zuckerberg",
+      url:
+        "https://media.vanityfair.com/photos/5de554a75e5bee0008b4ea54/master/pass/Zuck5.jpg",
+    },
+  ]);
+  return (
+    <div>
+      <h1>Tinder cards</h1>
+      {people.map((person) => (
+        <TinderCard
+          className="swipe"
+          key={person.name}
+          preventSwipe={["up", "down"]}
+        >
+          <div
+            style={{ backgroundImage: "url(${person.url})" }}
+            className="card"
+          >
+            <h3>{person.name}</h3>
+          </div>
+        </TinderCard>
+      ))}
+    </div>
+  );
+}
+
+export default TinderCards;
